f6c7cc6f515dca4f8c4388e4ce39d66db5b7a18a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-12-26 02:49:42 +0000

    Release 1.0.1

--- src/main/java/com/newscatcher/api/core/ClientOptions.java
+++ src/main/java/com/newscatcher/api/core/ClientOptions.java
@@ -33,7 +33,7 @@ public final class ClientOptions {
             {
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.newscatcher.fern:api-sdk");
-                put("X-Fern-SDK-Version", "1.0.0");
+                put("X-Fern-SDK-Version", "1.0.1");
             }
         });
         this.headerSuppliers = headerSuppliers;
